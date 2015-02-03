22755133b0ab904ac5e27485c154035350b6aa22
gmf <mg3534@columbia.edu>
2015-02-03 02:30:23 -0500

    modify models

---
+++ linkedcard/namecard/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+	user_id = models.CharField(max_length=50, primary_key=True)
+	temp_id = models.CharField(max_length=50)
+	qr_code_url = models.CharField(max_length=300)
+
+class Relation(models.Model):
+	user_id = models.ForeignKey(User)
